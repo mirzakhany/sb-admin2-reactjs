59ca61da9f4170e9cc08ffb1b3d0a9bb6a705460
mohsen <mohsen.mkh88@gmail.com>
2020-08-09 14:37:49 +0200

    topBar clean code

--- src/layouts/Admin/components/TopBar/topBar.js
+++ src/layouts/Admin/components/TopBar/topBar.js
@@ -20,7 +20,7 @@ const TopBar = props => {
         <nav className="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
             <div className="container-fluid">
                 <button className="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button">
-                    <i className="fas fa-bars"></i>
+                    <i className="fas fa-bars"/>
                 </button>
                 <form
                     className="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
@@ -29,7 +29,7 @@ const TopBar = props => {
                                placeholder="Search for ..."/>
                         <div className="input-group-append">
                             <button className="btn btn-primary py-0" type="button">
-                                <i className="fas fa-search"></i>
+                                <i className="fas fa-search"/>
                             </button>
                         </div>
                     </div>
@@ -37,7 +37,7 @@ const TopBar = props => {
                 <ul className="nav navbar-nav flex-nowrap ml-auto">
                     <li className="nav-item dropdown d-sm-none no-arrow">
                         <a className="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="/#">
-                            <i className="fas fa-search"></i></a>
+                            <i className="fas fa-search"/></a>
                         <div className="dropdown-menu dropdown-menu-right p-3 animated--grow-in" role="menu"
                              aria-labelledby="searchDropdown">
                             <form className="form-inline mr-auto navbar-search w-100">
@@ -46,14 +46,14 @@ const TopBar = props => {
                                            placeholder="Search for ..."/>
                                     <div className="input-group-append">
                                         <button className="btn btn-primary py-0" type="button">
-                                            <i className="fas fa-search"></i>
+                                            <i className="fas fa-search"/>
                                         </button>
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </li>
-                    <div className="d-none d-sm-block topbar-divider"></div>
+                    <div className="d-none d-sm-block topbar-divider"/>
                     <li className="nav-item dropdown no-arrow" role="presentation">
                         <div className="nav-item dropdown no-arrow">
                             <a className="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false"
@@ -63,10 +63,10 @@ const TopBar = props => {
                                      src="assets/img/avatars/avatar1.jpeg"/></a>
                             <div className="dropdown-menu shadow dropdown-menu-right animated--grow-in" role="menu">
                                 <Link className="dropdown-item" role="presentation" to="/profile">
-                                    <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>Profile</Link>
-                                <div className="dropdown-divider"></div>
+                                    <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"/>Profile</Link>
+                                <div className="dropdown-divider"/>
                                 <button className="dropdown-item" role="presentation" onClick={handleLogout}>
-                                    <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>Logout</button>
+                                    <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"/>Logout</button>
                             </div>
                         </div>
                     </li>
